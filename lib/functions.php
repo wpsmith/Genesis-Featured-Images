9e35de2f03e02412f522e4c5c290f1de901b1309
Travis Smith <t@wpsmith.net>
2015-09-02 17:32:14 -0400

    Fix featured image over-ride

--- lib/functions.php
+++ lib/functions.php
@@ -37,19 +37,23 @@ function gfi_get_image_data( $args ) {
 	);
 }
 
+function pr( $args ) {
+	echo '<pre>' . htmlspecialchars( print_r( $args, 1 ) ) . '</pre>';
+}
+
 /**
  * Filters genesis_get_image() returning the default image if html or url are empty.
  * 
  */
-add_filter( 'genesis_get_image_default_args', 'gfi_get_image_default_args', 10, 2 );
+//add_filter( 'genesis_get_image_default_args', 'gfi_get_image_default_args', 10, 2 );
 function gfi_get_image_default_args( $defaults, $args ) {
-	
 	if ( 'first-attached' !== $defaults['fallback'] || !genesis_get_option( 'featimg_default_enable' ) ) {
 		return $defaults;
 	}
-	
+
+	// Ok, now let's check to see if an image exists
 	$data = gfi_get_image_data( $args );
-	
+
 	return array(
 		'id'   => $data['id'],
 		'html' => $data['html'],
@@ -60,11 +64,17 @@ function gfi_get_image_default_args( $defaults, $args ) {
 
 add_filter( 'genesis_pre_get_image' , 'genesis_get_image_default' , 10 , 6 );
 function genesis_get_image_default( $pre, $args, $post ) {
-	
-	if ( $pre !== false || !genesis_get_option( 'featimg_default_enable' ) ) {
+
+	if ( $pre !== false || !genesis_get_option( 'featimg_default_enable' ) || has_post_thumbnail( $post->ID ) ) {
 		return $pre;
 	}
-	
+
+	// Get first-attached
+	if ( 'first-attached' === $args['fallback'] && genesis_get_image_id( $args['num'], $args['post_id'] ) ) {
+		return $pre;
+	}
+
+	// Now let's do our featured Image
 	$data = gfi_get_image_data( $args );
 	
 	// determine output
